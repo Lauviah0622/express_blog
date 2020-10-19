35e3790ecf584097c346ef4c489b21f5ff169180
Lauviah Fang <lauviah0622@gmail.com>
2020-10-19 11:22:22 +0800

    fix

--- seeders/20201015020728-test_article.js
+++ seeders/20201015020728-test_article.js
@@ -6,20 +6,27 @@ module.exports = {
       title: "關於我",
       content: "關於我",
       categoryId: 0,
-      
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
     await queryInterface.bulkInsert('Articles', [{
       title: "20/10/14",
       content: "今天早餐吃甚麼",
       categoryId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
 
     await queryInterface.bulkInsert('Categories', [{
       id: 0,
       name: "未分類",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
     await queryInterface.bulkInsert('Categories', [{
       name: "日記",
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
 
     /**
