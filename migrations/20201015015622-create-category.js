afbba5bf3f4ffef5d2d2125b8d5ba4135b89e3ed
Lauviah Fang <lauviah0622@gmail.com>
2020-10-19 10:57:34 +0800

    fix

--- migrations/20201015015622-create-category.js
+++ migrations/20201015015622-create-category.js
@@ -18,7 +18,8 @@ module.exports = {
       },
       updatedAt: {
         type: Sequelize.DATE,
-        allowNull: false
+        allowNull: false,
+        defaultValue: Sequelize.NOW
       }
     });
   },
