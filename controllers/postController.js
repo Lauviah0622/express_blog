6547af63254b2b8b91ba39a034fcf3f0b2b304b2
Lauviah Fang <lauviah0622@gmail.com>
2020-12-30 22:04:14 +0800

    ficed cate problem

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,12 +5,13 @@ const Category = db.Category;
 module.exports = {
     add: async (req, res) => {
         let {title, content, category: categoryId, newCate} = req.body;
-
+        let newCateId = null;
         if (newCate) {
-            var newCateId = await Category.create({newCate});
+            newCateData = await Category.create({name: newCate});
             // 這裡是不是只能用 var？ 
         }
-        console.log('newCateId', newCateId);
+        newCateId = newCateData.dataValues.id
+
         await Article.create({title, content, categoryId: newCateId || categoryId })
         res.redirect('/admin');
         
